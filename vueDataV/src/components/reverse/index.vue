8218d29802236374245e14c2b1c984bdd080302b
liuzy <liuzy@sweetmartmarketing.com>
2021-09-27 21:38:33 +0800

    demo

---
+++ vueDataV/src/components/reverse/index.vue
@@ -0,0 +1,123 @@
+<!--
+ 描述: 反转
+ -->
+<template>
+  <div class="wrap-container sn-container">
+    <div class="sn-content">
+      <div class="sn-title">反转</div>
+
+      <div class="main">
+        <div class="main-left">
+          <div class="completion">
+            <div class="completion-content">
+              <div class="rate">
+                <h5><span>完成率</span></h5>
+                <p>67%</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reverse",
+  data() {
+    return {
+      option: null,
+    };
+  },
+  mounted() {},
+  methods: {},
+  beforeDestroy() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.sn-container {
+  left: 880px;
+  top: 3300px;
+  width: 432px;
+  height: 600px;
+}
+
+.main {
+  width: 100%;
+  height: 100%;
+  padding: 5%;
+  position: relative;
+}
+.main .main-left .completion {
+  width: 100%;
+  height: 30%;
+  position: relative;
+  padding: 15%;
+}
+
+.main .main-left .completion .completion-content {
+  width: 17rem;
+  height: 17rem;
+  background: url(../../assets/img/reverse/border.png) no-repeat center;
+  background-size: contain;
+}
+
+.main .main-left .completion .completion-content .rate {
+  width: 14rem;
+  height: 14rem;
+  position: absolute;
+  left: 13%;
+  top: 51%;
+  transform: translateY(-50%);
+  background: url(../../assets/img/reverse/cricle.png) no-repeat center;
+  background-size: contain;
+  animation: rotate1 5s linear infinite;
+}
+
+.main .main-left .completion .completion-content .rate h5 {
+  text-align: center;
+  height: 50%;
+  width: 100%;
+  color: #fff;
+  display: table;
+}
+
+.main .main-left .completion .completion-content .rate h5 span {
+  display: table-cell;
+  vertical-align: bottom;
+  font-size: 1.2rem;
+}
+
+.main .main-left .completion .completion-content .rate p {
+  text-align: center;
+  height: 50%;
+  width: 100%;
+  color: #fff;
+  display: table;
+}
+
+.main .main-left .completion .completion-content .rate p span {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+@keyframes animate1 {
+  0% {
+    left: 50%;
+  }
+  100% {
+    left: 50%;
+  }
+}
+
+@keyframes rotate1 {
+  0% {
+    transform: translateY(-50%) rotateY(0);
+  }
+  100% {
+    transform: translateY(-50%) rotateY(360deg);
+  }
+}
+</style>
---
+++ vueDataV/src/components/suspendedBall/index.vue
@@ -0,0 +1,104 @@
+<!--
+ 描述: 悬浮球
+ -->
+<template>
+  <div class="wrap-container sn-container">
+    <div class="sn-content">
+      <div class="sn-title">悬浮球</div>
+      <div class="xuanfudata">
+        <div class="qiu">
+          <p>70%</p>
+        </div>
+        <span>成功率</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "suspendedBall",
+  data() {
+    return {
+      option: null,
+    };
+  },
+  mounted() {},
+  methods: {},
+  beforeDestroy() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.sn-container {
+  left: 974px;
+  top: 2838px;
+  width: 432px;
+  height: 400px;
+}
+
+.xuanfudata {
+  color: #fff;
+  text-align: center;
+  position: absolute;
+  width: 16rem;
+  height: 16rem;
+  background: url(../../assets/img/suspendedBall/di.png) no-repeat bottom center;
+  background-size: contain;
+  padding-top: 12%;
+  box-sizing: border-box;
+}
+
+.xuanfudata .qiu {
+  position: relative;
+  width: 8rem;
+  height: 8rem;
+  margin: auto;
+  display: table;
+}
+
+.xuanfudata .qiu p {
+  display: table-cell;
+  vertical-align: middle;
+  font-size: 1.8rem;
+}
+
+.xuanfudata .qiu::before {
+  content: "";
+  display: block;
+  width: 8rem;
+  height: 8rem;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  border: 3px solid rgba(255, 255, 255, 0.5);
+  animation: scale 2s linear infinite;
+}
+
+.xuanfudata span {
+  font-size: 1.2rem;
+}
+
+.xuanfudata {
+  left: 20%;
+  top: 10%;
+}
+
+.xuanfudata .qiu {
+  background: url(../../assets/img/suspendedBall/cicle02.png) no-repeat center;
+  background-size: 100%;
+}
+
+@keyframes scale {
+  0% {
+    transform: translate(-50%, -50%) scale(0.9);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(1.5);
+    opacity: 0;
+  }
+}
+</style>
